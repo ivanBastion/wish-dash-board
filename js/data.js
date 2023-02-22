d893228570780632702fc8ad09c1a3ad4f8a8986
ivanBastion <104712574+ivanBastion@users.noreply.github.com>
2023-02-22 19:51:36 +0100

    Add files via upload

--- js/data.js
+++ js/data.js
@@ -1,34 +1,46 @@
 const user = {
-  name: "Іван",
-  surname: "Шевченко",
+  name: "Ivan",
+  surname: "Bastion",
   userPhoto: "my-photo.jpg",
-  qrQode: "qr-code.svg",
+  qrQode: "qr-code.png",
   dreams: [
-    { src: "car.jpg", alt: "Авто моєї мрії" },
-    { src: "home.jpg", alt: "Домівка на місяці" },
-    { src: "voyage.jpg", alt: "Подорож до Марсу" },
+    { src: "car.jpg", alt: "lamborgini aventador" },
+    { src: "home.jpg", alt: "Домівка на берегу моря" },
+    { src: "voyage.jpg", alt: "Подорож до сеула" },
   ],
 };
 
 // arr це середня місячна платня по рокам
 const arr = [
+  200,
+  300,
   500,
-  700,
   800,
-  1000,
+  1100,
   1200,
   1500,
   1800,
   2100,
-  2400,
-  2700,
+  2500,
+  2550,
+  2850,
   3000,
-  3300,
+  3500,
+  3900,
+  4300,
+  4700,
+  5100,
+  5500
 ];
 
 const necessaryExpenses = 800;
 
 const todo = [
+  { month: "Січень", skill: "Лідерство" },
+  { month: "Лютий", skill: "Нетворкінг" },
+  { month: "Березень", skill: "Особистий бренд" },
+  { month: "Квітень", skill: "Управління фінансами" },
+  { month: "Травень", skill: "Публічні виступи" },
   { month: "Червень", skill: "JavaScript" },
   { month: "Липень", skill: "Java" },
   { month: "Серпень", skill: "Python" },
@@ -36,11 +48,6 @@ const todo = [
   { month: "Жовтень", skill: "Гнучкість" },
   { month: "Листопад", skill: "Емоційний інтелект" },
   { month: "Грудень", skill: "Креативність" },
-  { month: "Січень", skill: "Лідерство" },
-  { month: "Лютий", skill: "Нетворкінг" },
-  { month: "Березень", skill: "Особистий бренд" },
-  { month: "Квітень", skill: "Управління фінансами" },
-  { month: "Травень", skill: "Публічні виступи" },
 ];
 
 export { user, arr, necessaryExpenses, todo };
